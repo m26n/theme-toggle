e9049d8fea9e55713d47cdbcd2a8206ff1b52b42
Maurice Bergerhausen <mbn@fastmail.de>
2025-04-08 22:43:11 +0200

    prettify text

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function Box({ title }: { title: string }) {
   return (
     <div
       data-title={title}
-      className="after:bg-light-blue dark:after:bg-dark-blue red:after:bg-dark-red relative max-w-120 rounded-sm border-2 p-4 after:absolute after:top-0 after:right-0 after:-translate-x-4 after:-translate-y-1/2 after:px-2 after:content-[attr(data-title)]"
+      className="after:bg-light-blue dark:after:bg-dark-blue red:after:bg-dark-red relative max-w-120 rounded-sm border-2 p-4 after:absolute after:top-0 after:right-0 after:-translate-x-4 after:-translate-y-1/2 after:px-2 after:content-[attr(data-title)] even:text-pretty"
     >
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
       officiis eligendi ex minima delectus fuga quo voluptates ullam harum
